5bc8f34e33def268e11494cb8ab04854a1c49666
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-13 15:54:36 +0400

    finished with navbar. Constructing welcome section

---
+++ Artopia/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import WelcomePage from './components/WelcomePage/WelcomePage'
+import Navigation from './components/Navigation';
+
+function App() {
+
+  return (
+    <>
+    <Navigation />
+      <WelcomePage />
+    </>
+  )
+}
+
+export default App
---
+++ Artopia/src/components/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function WelcomePage() {
+    return <section id="welcome_section">
+            
+        </section>
+}
