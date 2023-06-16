45cf9454d4970078fd816a1563a5127d059fdfc6
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-16 18:37:55 +0400

    Improved visual of carousel. Added countup

--- src/components/WelcomePage/StatisticList.jsx
+++ src/components/WelcomePage/StatisticList.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import Statistic from "./Statistic"
+import CountUp from "react-countup";
 
 export default function StatisticList() {
-        return (<div className="statistic_list">
-                <Statistic num={"999,000"} category={"Digital art file"} />
-                <Statistic num={"2,000"} category={"Art Seller"} />
-                <Statistic num={"1,0000"} category={"Buyer"} />
-            </div>)
+
+  return (
+    <div className="statistic_list">
+      <div className="statistic_component">
+        <h4><CountUp start={0} end={2000} duration={3} separator="," ></CountUp></h4>
+        <p className="text_color_an">Digital art file</p>
+      </div>
+      <div className="statistic_component">
+        <h4><CountUp start={0} end={999000} duration={5} separator="," ></CountUp></h4>
+       
+        <p className="text_color_an">Art Seller</p>
+      </div>
+      <div className="statistic_component">
+        <h4><CountUp start={0} end={1000} duration={2} separator="," ></CountUp></h4>
+        <p className="text_color_an">Buyer</p>
+      </div>
+    </div>
+  );
 }
